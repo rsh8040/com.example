61bebe1a2774c93c5f27403638baed2e70f9977c
임수현 <shlim@blatform.co.kr>
2023-07-26 15:18:54 +0900

    패키지 구조변경

--- src/CasttingEx1.java
+++
@@ -1,9 +0,0 @@
-public class CasttingEx1 {
-    public static void main(String[] args) {
-        double d = 85.4;
-        int score = (int)d;
-
-        System.out.println("score=" + score);
-        System.out.println("d=" + d);
-    }
-}
